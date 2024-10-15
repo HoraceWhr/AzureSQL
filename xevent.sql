794c8d32bad281a6d8cb9fd62b3bfb7743a5c84b
HoraceWhr <66395782+HoraceWhr@users.noreply.github.com>
2024-10-15 21:33:17 +0800

    Update xevent.sql

--- xevent.sql
+++ xevent.sql
@@ -3,7 +3,7 @@ Please follow below steps to collect the trace：
 2.run steps 1-3 to start the session
 3. repro the issue
 4. run step 4 to stop the session
-5.upload the event logs in blob container 
+5.upload the event logs 
  
 
 
